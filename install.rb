d5a885be1bb1f80f8cac3931608ba58a224abe22
Mark Evans <mark@new-bamboo.co.uk>
2009-02-20 11:40:13 +0000

    More helpful install feedback

--- install.rb
+++ install.rb
@@ -8,8 +8,16 @@ asset_dir = "#{File.dirname(__FILE__)}/public"
   "#{asset_dir}/stylesheets/jquery.partial_identifier.css" => PartialIdentifier.css_file_path
 }.each do |from, to|
   
-  puts "Moving #{File.basename(from)} to #{to}..."
-  FileUtils.mv from, to
-  puts "...done"
-
+  puts "Copying #{File.basename(from)} to #{to}..."
+  begin
+    FileUtils.mv from, to
+    puts "...done"
+  rescue
+    puts '...failed. Please copy this file manually from the plugin to the appropriate place.'
+  end
+  puts "**************"
+  puts "If you need the js/css files to be in a different place, you can modify the following config attributes:"
+  puts "PartialIdentifier.js_file_path, PartialIdentifier.css_file_path, PartialIdentifier.js_url_path and PartialIdentifier.css_url_path."
+  puts "**************"
+  
 end
