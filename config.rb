41254c5f2fd0b7082ba537f7640bfead4c8226b3
Mark Evans <mark@new-bamboo.co.uk>
2009-02-20 11:25:15 +0000

    Fixed stupid falta de self.attr_accessor in PI config

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ module PartialIdentifier
     attr_accessor :js_url_path, :css_url_path, :js_file_path, :css_file_path
   end
 
-  js_url_path  = "/javascripts"
-  css_url_path = "/stylesheets"
+  self.js_url_path  = "/javascripts"
+  self.css_url_path = "/stylesheets"
   
-  js_file_path  = "#{RAILS_ROOT}/public#{js_url_path}"
-  css_file_path = "#{RAILS_ROOT}/public#{css_url_path}"
+  self.js_file_path  = "#{RAILS_ROOT}/public#{js_url_path}"
+  self.css_file_path = "#{RAILS_ROOT}/public#{css_url_path}"
   
 end
--- install.rb
+++ install.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 # Put js/css files in the appropriate directories
 asset_dir = "#{File.dirname(__FILE__)}/public"
 {
-  "#{asset_dir}/javascripts/jquery.partial_identifier.js" => PartialIdentifier.js_file_path,
+  "#{asset_dir}/javascripts/jquery.partial_identifier.js"  => PartialIdentifier.js_file_path,
   "#{asset_dir}/stylesheets/jquery.partial_identifier.css" => PartialIdentifier.css_file_path
 }.each do |from, to|
   
