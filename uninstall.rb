a10b08bef8027ec33346549e285876bcd923858b
Mark Evans <mark@new-bamboo.co.uk>
2009-02-20 11:30:24 +0000

    Added uninstall code

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,15 @@
-# Uninstall hook code here
+require "#{File.dirname(__FILE__)}/config"
+require 'fileutils'
+
+# Remove js/css files from the appropriate directories
+[ PartialIdentifier.js_file_path, PartialIdentifier.css_file_path ].each do |file|
+  
+  puts "Removing #{file}..."
+  begin
+    FileUtils.rm file
+    puts "...done"
+  rescue
+    puts '...failed'
+  end
+
+end
