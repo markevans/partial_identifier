a664c5f1bd69ccb4cc3aa6ebd79894675a542d05
Mark Evans <mark@new-bamboo.co.uk>
2009-02-20 11:08:46 +0000

    Added install code to copy files into public directory

--- install.rb
+++ install.rb
@@ -1,17 +1,15 @@
-# Install hook code here
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-puts 'TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
+require "#{File.dirname(__FILE__)}/config"
+require 'fileutils'
+
+# Put js/css files in the appropriate directories
+asset_dir = "#{File.dirname(__FILE__)}/public"
+{
+  "#{asset_dir}/javascripts/jquery.partial_identifier.js" => PartialIdentifier.js_file_path,
+  "#{asset_dir}/stylesheets/jquery.partial_identifier.css" => PartialIdentifier.css_file_path
+}.each do |from, to|
+  
+  puts "Moving #{File.basename(from)} to #{to}..."
+  FileUtils.mv from, to
+  puts "...done"
+
+end
