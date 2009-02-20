8e84372bb0d66051c68f8ac7aa6085f6848ba2dc
Mark Evans <mark@new-bamboo.co.uk>
2009-02-20 11:35:09 +0000

    Fixed incorrect uninstall filename

--- uninstall.rb
+++ uninstall.rb
@@ -2,14 +2,17 @@ require "#{File.dirname(__FILE__)}/config"
 require 'fileutils'
 
 # Remove js/css files from the appropriate directories
-[ PartialIdentifier.js_file_path, PartialIdentifier.css_file_path ].each do |file|
+[
+  "#{PartialIdentifier.js_file_path}/jquery.partial_identifier.js",
+  "#{PartialIdentifier.css_file_path}/jquery.partial_identifier.css",
+].each do |file|
   
   puts "Removing #{file}..."
   begin
     FileUtils.rm file
     puts "...done"
   rescue
-    puts '...failed'
+    puts '...failed. Please remove this file manually.'
   end
 
 end
