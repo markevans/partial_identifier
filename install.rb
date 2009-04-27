cb71aa4db87b42fa79134b505bdfe0b73f7eb160
Mark Evans <mark@new-bamboo.co.uk>
2009-04-27 14:28:14 +0100

    Corrected install (wasn't copying jquery)

--- install.rb
+++ install.rb
@@ -7,7 +7,7 @@ require 'fileutils'
 puts "Making sure directory #{PartialIdentifier::FILE_PATH} exists"
 FileUtils.mkdir_p(PartialIdentifier::FILE_PATH)
 
-%w{partial_identifier.js partial_identifier.css}.each do |file|
+%w{partial_identifier.js partial_identifier.css jquery-1.3.2.min.js}.each do |file|
   
   src = "#{File.dirname(__FILE__)}/public/partial_identifier/#{file}"
   dest = PartialIdentifier::FILE_PATH
@@ -30,5 +30,8 @@ puts "#{PartialIdentifier::FILE_PATH.sub(RAILS_ROOT,'').sub(%r(^/),'')}"
 puts "vendor/plugins/partial_identifier"
 puts
 puts " to your ignore file"
+puts
+puts "To remove:"
+puts "    script/plugin remove vendor/plugins/partial_identifier"
 puts
 puts "**************"
