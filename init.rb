d5685a727dd4188743e3629cf69459a2d21f8186
Mark Evans <mark@new-bamboo.co.uk>
2009-04-27 14:31:56 +0100

    Fixed renamed file require

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 if RAILS_ENV == 'development'
   jpi_root = File.dirname(__FILE__)
   require "#{jpi_root}/config"
-  require "#{jpi_root}/lib/action_view/template"
+  require "#{jpi_root}/lib/action_view/jpi_template"
 end
