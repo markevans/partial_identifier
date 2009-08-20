914c9ca5933e5bf289ba20a918d7d4bab2447096
Mark Evans <mark@new-bamboo.co.uk>
2009-08-20 11:57:46 +0100

    Added warning when it detects firebug (slows it down), also styled it a bit better

--- public/partial_identifier/partial_identifier.js
+++ public/partial_identifier/partial_identifier.js
@@ -4,10 +4,10 @@
 
   $.count = function(array, arg){
     if(typeof(arg) == 'function') callback = arg;
-    else if(typeof(arg) == 'undefined') callback = function(){return true};
-    else callback = function(n){return n == arg};
+    else if(typeof(arg) == 'undefined') callback = function(){return true;};
+    else callback = function(n){return n == arg;};
     
-    return $.grep(array, callback).length
+    return $.grep(array, callback).length;
   };
 
   $.fn.extend({
@@ -37,8 +37,8 @@
     
     partialStack : function(){
       // Get all the previous opened and closed partials
-      var opened_partials = $(this).prevALL('._jpi_start').map(function(){return this.id});
-      var closed_partials = $(this).prevALL('._jpi_end').map(function(){return this.id});
+      var opened_partials = $(this).prevALL('._jpi_start').map(function(){return this.id;});
+      var closed_partials = $(this).prevALL('._jpi_end').map(function(){return this.id;});
       // The stack is the opened partials, minus any closed ones
       var stack = [];
       var num_opened = null;
@@ -56,37 +56,49 @@
   
     partialStackAsList : function(){
       var lis = $.map($(this).partialStack(), function(partial_path){
-        var file_parts = partial_path.split('/')
-        var text = file_parts[file_parts.length-1]
-        return '<li><a class="_jpi_link" href="txmt://open/?url=file://' + partial_path + '">' + text + '</a></li>'
-      }).reverse().join("\n")
-      return '<ul>' + lis + '</ul>'
+        var file_parts = partial_path.split('/');
+        var text = file_parts[file_parts.length-1];
+        return '<li><a class="_jpi_link" href="txmt://open/?url=file://' + partial_path + '">' + text + '</a></li>';
+      }).reverse().join("\n");
+      return '<ul>' + lis + '</ul>';
     }
   
   });
 
   var display = function(message){
     if($('#_jpi_message_box').length == 0) {
-      $('body').append('<div id="_jpi_message_box"></div>')
+      $('body').append('<div id="_jpi_message_box"><h3>Partial identifier</h3><div></div></div>');
     }
-    $('#_jpi_message_box').html(message)
-  }
+    $('#_jpi_message_box div').html(message);
+  };
+
+  var warn = function(message){
+    if($('#_jpi_warning_message_box').length == 0) {
+      $('body').append('<div id="_jpi_warning_message_box"></div>');
+    }
+    var box = $('#_jpi_warning_message_box');
+    box.html(message).css({left: ($(window).width()/2.0 - box.width()/2.0) });
+  };
+  
+  var removeWarning = function(){
+    $('#_jpi_warning_message_box').fadeOut(8000, function(){$(this).remove();});
+  };
   
   var turnOffLinks = function(){
     return false;
-  }
+  };
   
   var updateTemplateList = function(evt){
     display( $(evt.target).partialStackAsList() );
     return true;
-  }
+  };
   
   var toggleJpi = function(){
     
     if(jpi_on){ // Turn OFF
       
-      display('Partial Identifier OFF');
-      $('#_jpi_message_box').fadeOut(3000, function(){$(this).remove()});
+      display('Turning OFF');
+      $('#_jpi_message_box').fadeOut(3000, function(){$(this).remove();});
       $('*').unbind('click', updateTemplateList);
       $('*').unbind('click', turnOffLinks);
       $('body').removeClass('jpi_on');
@@ -94,22 +106,25 @@
 
     } else { // Turn ON
       
+      if($('#_firebugConsole').length > 0) warn("Make sure the firebug panel is closed<br />as this makes partial identifier slower<br />when turning on");
       $('body').addClass('jpi_on');
       $('a, input[type=submit], button').click(turnOffLinks);
       $('*').click(updateTemplateList);
-      display('Partial Identifier ON');
-      jpi_on = true
+      setTimeout(function(){},5000);
+      display('');
+      removeWarning();
+      jpi_on = true;
       
     }
-  }
+  };
   
   // Start the partial identifier when you press certain keys
   $(window).keydown(function (e) {
     // If the key combo is Ctrl + P...
     if(e.which == 17) isCtrl=true;
     if(e.which == 80 && isCtrl == true) {
-      toggleJpi()
+      toggleJpi();
     }
-  })
+  });
 
 })(jQuery);
